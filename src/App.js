af59912914bc7e3c06393a8ec0e8948f5fc2710a
Moses <mosngamau@>
2025-04-08 16:19:44 +0300

    Initial commit with working React app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to HoAGDP Project</h1>
+      <p>Your application is successfully deployed!</p>
+    </div>
+  );
+}
+
+export default App;
